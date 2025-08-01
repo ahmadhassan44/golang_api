6d7db91d77318f112be06a0f7cf770a7ad79764f
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2025-08-02 03:14:24 +0500

    feat/setup models

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type Course struct {
+	CourseId    int     `json:"id"`
+	CourseName  string  `json:"name"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Website   string `json:"website"`
+}
+
 func main() {
 	fmt.Println("hello")
 }
