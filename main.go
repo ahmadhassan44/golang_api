3773f06c9c3a5f73e9257cef404c092615221889
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2025-08-02 03:18:48 +0500

    feat/add middleware

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import "fmt"
 
 type Course struct {
-	CourseId    int     `json:"id"`
+	CourseId    string  `json:"id"`
 	CourseName  string  `json:"name"`
 	CoursePrice int     `json:"price"`
-	Author      *Author `json"author"`
+	Author      *Author `json:"author"`
 }
 
 type Author struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	Website   string `json:"website"`
+}
+
+var courses []Course
+
+//middlewares
+
+func IsEmpty(course *Course) bool {
+	return course.CourseId == "" && course.CourseName == ""
 }
 
 func main() {
