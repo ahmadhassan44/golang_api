28797bfae10e02d82113484fd7fa71b848d34622
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2025-08-06 02:05:03 +0500

    feat/add routes for handling db operations

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module example.com/golang_api
 
 go 1.24.4
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
 
 type Course struct {
 	CourseId    string  `json:"id"`
 	CourseName  string  `json:"name"`
 	CoursePrice int     `json:"price"`
 	Author      *Author `json:"author"`
+}
+
+func (course Course) IsEmpty() bool {
+	return course.CourseName == ""
 }
 
 type Author struct {
@@ -25,4 +36,84 @@ func IsEmpty(course *Course) bool {
 
 func main() {
 	fmt.Println("hello")
+}
+
+//controllers
+
+// herve home route
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to my golang api</h1>"))
+}
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+func getCourseById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Course not found"})
+}
+func addCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Body == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Please send course body to add course!"})
+		return
+	}
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "Please send course body to add course!"})
+		return
+	}
+	course.CourseId = uuid.New().String()
+	courses = append(courses, course)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(course)
+}
+func updateCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Body == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	params := mux.Vars(r)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+}
+func deleteCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	if params["id"] == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "No ID provided"})
+		return
+	}
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
 }
